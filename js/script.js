558b9426c20498955820db9e6776f0a166062f75
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-09 16:01:05 +0200

    fixed 'document e aggiunta un po' di grafica

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-//todo arrivato fino a qui
-
 // BONUS: (da fare solo se funziona tutto il resto) all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali: con difficoltà 0 => tra 1 e 100 con difficoltà 1 => tra 1 e 80 con difficoltà 2 => tra 1 e 50
 
 // Consigli del giorno:
@@ -16,7 +14,7 @@ var elabora = document.getElementById('bInizia');
 // *pulsante inizia
 elabora.addEventListener("click", function(){
 
-    var lev = documet.getElementById('lev').value;    
+    var lev = document.getElementById('lev').value;    
     
     //genera 16 numeri casuali da 1 a lev(100,80,50)
     var numeriPc = [];
@@ -39,9 +37,9 @@ elabora.addEventListener("click", function(){
     {
         var numUser = parseInt(prompt('inserisci un numero da 1 a '+lev));
         if (numeriPc.includes(numUser)){
-            console.log('hai perso, numero presente nella lista numeri del pc, il tuo risultato è: ' + arrayUser.length);
+            alert('hai perso, numero presente nella lista numeri del pc, il tuo risultato è: ' + arrayUser.length);
         } else if (arrayUser.includes(numUser)){
-            console.log('hai già inserito questo numero, riprova');
+            alert('hai già inserito questo numero, riprova');
         } else if(!numeriPc.includes(numUser) && !arrayUser.includes(numUser)) //se numero utente non presente in numeriPc e in numeri arrayUser
         {
             arrayUser.push(numUser); //mettili dentro array user
@@ -49,10 +47,8 @@ elabora.addEventListener("click", function(){
     }
     
     if (arrayUser.length == level(lev)) {
-        console.log('hai vinto');
+        console.log('Hai vinto');
     }
-
-
 
 });
 
